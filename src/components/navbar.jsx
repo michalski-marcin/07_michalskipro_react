64fde03ff65e08c3ca14f15324e9c8c34633d28a
Marcin Michalski <mkamichalski@gmail.com>
2023-12-03 14:13:46 +0100

    0.0.0.1
    Layout draft

---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import './navbar.css'
+
+function Navbar() {
+    return (
+      <>
+       <nav>
+        <div className="logo">logo</div>
+        <div className="menu">menu</div>
+       </nav>
+      </>
+    )
+  }
+
+  export default Navbar
