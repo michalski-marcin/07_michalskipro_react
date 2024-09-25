2060d4b2681f1ccdc98bbbd86a0ae63b6e82c593
Marcin Michalski <mkamichalski@gmail.com>
2024-09-25 21:14:28 +0200

    1.0.7 Styled post page

--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -1,8 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useParams, Link } from 'react-router-dom';
 import { getPosts } from './getData';  // Ensure getPosts is correctly defined
 import '../styles/blog.css';
 import '../styles/post.css';
+import Prism from 'prismjs';
+import 'prismjs/components/prism-java';
+import 'prismjs/themes/prism.css'; // Prism theme for syntax highlighting
 
 function PostDetail() {
   const { id } = useParams();  // Get the post ID from the URL
@@ -30,6 +33,13 @@ function PostDetail() {
     fetchPost();  // Call the fetch function
   }, [id]);
 
+  // Apply PrismJS syntax highlighting after the post content is loaded
+  useEffect(() => {
+    if (post) {
+      Prism.highlightAll();  // Apply syntax highlighting
+    }
+  }, [post]);
+
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -39,9 +49,19 @@ function PostDetail() {
   }
 
   return (
-    <div className='wrap'>
-      <h2>{post.title.rendered}</h2>
+    <div className='wrap post-wrap'>
+      {/* Link back to the posts list */}
+      <Link to='/blog' className='back-link'>← Back to Posts</Link>
+
+      {/* Display the featured image if available */}
+      {post.featured_media_url && <img src={post.featured_media_url} alt={post.title.rendered} className='featured-image' />}
+
+      <h2 dangerouslySetInnerHTML={{ __html: post.title.rendered }} />
+      {/* Display content while keeping HTML tags intact */}
       <div className='post-content' dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
+
+      {/* Link back to the posts list at the bottom */}
+      <Link to='/blog' className='back-link'>← Back to Posts</Link>
     </div>
   );
 }
