dd5a3cae52414523de69363ccf19d793537fea4f
Marcin Michalski <mkamichalski@gmail.com>
2024-09-30 21:33:21 +0200

    skills page styling

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,48 +1,4 @@
-
-
-export const frontend = [
-    {
-        id: 1,
-        image: 'react-original.svg',
-        name: 'React',
-        
-
-    },
-    {
-        id: 2,
-        image: 'typescript-original.svg',
-        name: 'TypeScript',
-    },
-    {
-        id: 3,
-        image: 'javascript-original.svg',
-        name: 'JavaScript',
-    },
-    {
-        id: 4,
-        image: 'tailwindcss-plain.svg',
-        name: 'Tailwind',
-    },
-    {
-        id: 5,
-        image: 'sass-original.svg',
-        name: 'Sass',
-    },
-    {
-        id: 6,
-        image: 'css3-original.svg',
-        name: 'CSS',
-    },
-    {
-        id: 7,
-        image: 'html5-original.svg',
-        name: 'HTML',
-    },
-    
-
-];
-
-export const backend = [
+export const programmingLanguages = [
     {
         id: 1,
         image: 'java-original.svg',
@@ -50,19 +6,73 @@ export const backend = [
     },
     {
         id: 2,
+        image: 'javascript-original.svg',
+        name: 'JavaScript',
+    },
+    {
+        id: 3,
+        image: 'typescript-original.svg',
+        name: 'TypeScript',
+    },
+];
+
+export const frameworks = [
+    {
+        id: 1,
         image: 'spring-original.svg',
         name: 'Spring',
     },
     {
+        id: 2,
+        image: 'springboot.svg',
+        name: 'Spring Boot',
+    },
+    {
         id: 3,
+        image: 'JUnit.svg',
+        name: 'JUnit',
+    },
+    {
+        id: 4,
+        image: 'react-original.svg',
+        name: 'React',
+    },
+];
+
+export const databasesAndMessaging = [
+    {
+        id: 1,
         image: 'mysql-original.svg',
         name: 'MySQL',
     },
-    // {
-    //     id: 4,
-    //     image: 'nodejs-original.svg',
-    //     name: 'MySQL',
-    // },
+    {
+        id: 2,
+        image: 'apache_kafka.svg',
+        name: 'Kafka',
+    },
+];
+
+export const markupAndStyling = [
+    {
+        id: 1,
+        image: 'html5-original.svg',
+        name: 'HTML',
+    },
+    {
+        id: 2,
+        image: 'css3-original.svg',
+        name: 'CSS',
+    },
+    {
+        id: 3,
+        image: 'sass-original.svg',
+        name: 'Sass',
+    },
+    {
+        id: 4,
+        image: 'tailwindcss-plain.svg',
+        name: 'Tailwind',
+    },
 ];
 
 export const tools = [
@@ -78,27 +88,37 @@ export const tools = [
     },
     {
         id: 3,
+        image: 'docker-original.svg',
+        name: 'Docker',
+    },
+    {
+        id: 4,
+        image: 'kubernetes-plain.svg',
+        name: 'Kubernetes',
+    },
+    {
+        id: 5,
+        image: 'amazonwebservices-original.svg',
+        name: 'AWS',
+    },
+    {
+        id: 6,
+        image: 'maven-original.svg',
+        name: 'Maven',
+    },
+    {
+        id: 7,
+        image: 'gradle-plain.svg',
+        name: 'Gradle',
+    },
+    {
+        id: 8,
         image: 'intellij.svg',
         name: 'IntelliJ',
     },
     {
-        id: 4,
+        id: 9,
         image: 'vscode-original.svg',
         name: 'VSCode',
-    },
-    {
-        id: 4,
-        image: 'figma-original.svg',
-        name: 'Figma',
-    },
-    {
-        id: 5,
-        image: 'canva-original.svg',
-        name: 'Canva',
-    },
-    {
-        id: 6,
-        image: 'photoshop-plain.svg',
-        name: 'Photoshop',
     },
 ];
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,7 +1,7 @@
 import '../styles/page.css';
 import { motion } from 'framer-motion';
 import Skill from '../components/Skill';
-import { frontend, backend, tools } from '../data/skills';
+import { programmingLanguages, frameworks, databasesAndMessaging, markupAndStyling, tools } from '../data/skills';
 
 function Skills() {
   return (
@@ -13,10 +13,27 @@ function Skills() {
             animate={{ opacity: 200, x: 0 }}
             exit={{ opacity: 0, x: -200 }}
             transition={{ duration: 0.5, ease: [0.445, 0.05, 0.55, 0.95] }}>
-            <div className='skills-title'>// BACK END</div>
+            <div className='skills-title'>// Programming Languages</div>
           </motion.div>
           <div className='skills-container'>
-            <Skill skills={backend} />
+            <Skill skills={programmingLanguages} />
+          </div>
+        </div>
+
+        <div className='skills-section'>
+          <motion.div
+            initial={{ opacity: 0, x: 200 }}
+            animate={{ opacity: 200, x: 0 }}
+            exit={{ opacity: 0, x: -200 }}
+            transition={{
+              duration: 0.5,
+              ease: [0.445, 0.05, 0.55, 0.95],
+              delay: 0.1,
+            }}>
+            <div className='skills-title'>// Frameworks</div>
+          </motion.div>
+          <div className='skills-container'>
+            <Skill skills={frameworks} />
           </div>
         </div>
 
@@ -30,10 +47,27 @@ function Skills() {
               ease: [0.445, 0.05, 0.55, 0.95],
               delay: 0.2,
             }}>
-            <div className='skills-title'>// FRONT END</div>
+            <div className='skills-title'>// Databases and Messaging</div>
           </motion.div>
           <div className='skills-container'>
-            <Skill skills={frontend} />
+            <Skill skills={databasesAndMessaging} />
+          </div>
+        </div>
+
+        <div className='skills-section'>
+          <motion.div
+            initial={{ opacity: 0, x: 200 }}
+            animate={{ opacity: 200, x: 0 }}
+            exit={{ opacity: 0, x: -200 }}
+            transition={{
+              duration: 0.5,
+              ease: [0.445, 0.05, 0.55, 0.95],
+              delay: 0.3,
+            }}>
+            <div className='skills-title'>// Markup and Styling</div>
+          </motion.div>
+          <div className='skills-container'>
+            <Skill skills={markupAndStyling} />
           </div>
         </div>
 
@@ -47,7 +81,7 @@ function Skills() {
               ease: [0.445, 0.05, 0.55, 0.95],
               delay: 0.4,
             }}>
-            <div className='skills-title'>// TOOLS</div>
+            <div className='skills-title'>// Tools</div>
           </motion.div>
           <div className='skills-container'>
             <Skill skills={tools} />
